833c9cd39dde4193416e471dac38fb3481b0ab4b
Jaime Herrera <jhdevworks@gmail.com>
2026-01-13 01:28:33 -0300

    Added a button to generate correlative example files.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,29 @@
 import { Text, View, Button, Alert, Platform } from "react-native";
 import * as FileSystem from "expo-file-system";
-import { File, Paths } from 'expo-file-system';
+import { File, Paths, Directory } from 'expo-file-system';
 
 
 
 async function generateFile() {
   try {
-    // Option B: create in cache then move to documents
-    const cacheFile = new File(Paths.cache, "example.txt");
+    let exampleIndex = 0;
+    let formattedNumber = String(exampleIndex).padStart(2, "0");
+    let candidateFilename = `example_${formattedNumber}.txt`;
+    const documentsDirectory = Paths.document.uri;
+    do {
+      const candidateUri = `${documentsDirectory}/${candidateFilename}`;
+      const candidateFile = new File(candidateUri);
+      const info = candidateFile.info();
+      if (!info.exists) break;
+      formattedNumber = String(exampleIndex).padStart(2, "0");
+      candidateFilename = `example_${formattedNumber}.txt`;
+      exampleIndex++;
+    } while (true)
+
+    const cacheFile = new File(Paths.cache, candidateFilename);
     await cacheFile.create();
     await cacheFile.write("Hello, world!");
-    const docFile = new File(Paths.document, "example.txt");
+    const docFile = new File(Paths.document, candidateFilename);
     await cacheFile.move(docFile);
 
     const msg = `Saved to ${docFile.uri}`;
